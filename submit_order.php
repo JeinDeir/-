f796ea3238e028244036e8d60655a31f7e7c9e12
JeinDeir <permyakovvitaliy398@gmail.com>
2024-10-26 10:34:02 +0900

    Add files via upload

--- submit_order.php
+++ submit_order.php
@@ -1,41 +1,35 @@
 <?php
-// 1. Параметры для подключения к базе данных SQL Server
-$serverName = "localhost"; // Имя сервера SQL
+$serverName = "DESKTOP-9N1TE4G\SQLEXPRESS"; 
 $connectionOptions = array(
-    "Database" => "confectionery_orders", // Имя базы данных
-    "Uid" => "root", // Имя пользователя базы данных
-    "PWD" => "" // Пароль пользователя
+    "Database" => "OrdersDB",
+    "Uid" => "имя_пользователя",
+    "PWD" => "пароль"
 );
 
-// 2. Устанавливаем соединение
+// Установка соединения
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 
-// 3. Проверка соединения
 if ($conn === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 
-// 4. Получение данных из формы
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
-$order_details = $_POST['order_details'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $orderDetails = $_POST['order'];
 
-// 5. SQL-запрос для вставки данных в таблицу
-$sql = "INSERT INTO orders (name, phone, email, order_details) VALUES (?, ?, ?, ?)";
+    $query = "INSERT INTO Orders (Name, Phone, Email, OrderDetails) VALUES (?, ?, ?, ?)";
+    $params = array($name, $phone, $email, $orderDetails);
+    $stmt = sqlsrv_query($conn, $query, $params);
 
-// 6. Подготовка и выполнение запроса
-$params = array($name, $phone, $email, $order_details);
-$stmt = sqlsrv_query($conn, $sql, $params);
+    if ($stmt) {
+        echo "Заказ успешно отправлен!";
+    } else {
+        echo "Ошибка: " . print_r(sqlsrv_errors(), true);
+    }
 
-// 7. Проверка выполнения запроса
-if ($stmt === false) {
-    die(print_r(sqlsrv_errors(), true));
-} else {
-    echo "Заявка успешно отправлена!";
+    sqlsrv_free_stmt($stmt);
 }
-
-// 8. Закрытие соединения
-sqlsrv_free_stmt($stmt);
 sqlsrv_close($conn);
 ?>
